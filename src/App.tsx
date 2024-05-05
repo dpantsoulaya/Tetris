d10dfc9ff19671c7165c4c80699087ac802f935c
dpantsoulaya <david@holyhope.ru>
2024-05-06 01:18:18 +0300

    Работа над поворотом фигуры

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
-import { SplitLayout, SplitCol, Text, FixedLayout, Div } from "@vkontakte/vkui";
+import { SplitLayout, SplitCol, Text, FixedLayout, Div, Button, ButtonGroup } from "@vkontakte/vkui";
 
 import "./app.css";
 
 import { Point } from "./types/point";
 import { Block } from "./types/block";
 
+const FIELD_SIZE_X = 10;
+const FIELD_SIZE_Y = 20;
 const BLOCK_SIZE = 25;
 
 export const App = () => {
@@ -22,14 +24,9 @@ export const App = () => {
     });
   };
 
-  const handleKeyPress = (event: KeyboardEvent) => {
-    console.log(event.key);
-    if (event.key == "ArrowLeft") {
-      moveLeft();
-    } else if (event.key == "ArrowRight") {
-      moveRight();
-    }
-  };
+  useEffect(() => {
+    console.log(currentBlock);
+  }, [currentBlock]);
 
   const moveLeft = () => {
     const new_points: Point[] = currentBlock.points.map((p) => {
@@ -45,10 +42,53 @@ export const App = () => {
     setCurrentBlock({ points: new_points });
   };
 
+  // поворот блока
+  const rotate = () => {
+    console.log("rotating block");
+    const avg_x = Math.round(currentBlock.points.map((p) => p.x).reduce((sum, p) => sum + p) / 4);
+    const avg_y = Math.round(currentBlock.points.map((p) => p.y).reduce((sum, p) => sum + p) / 4);
+
+    console.log(`avg x = ${avg_x} ; avg_y = ${avg_y}`);
+
+    const new_points: Point[] = currentBlock.points.map((p) => {
+      //debugger;
+
+      if (p.x == avg_x && p.y == avg_y) {
+        return { x: p.x, y: p.y };
+      } else if (p.x == avg_x && p.y < avg_y) {
+        return { x: p.x + 1, y: p.y + 1 };
+      } else if (p.x == avg_x && p.y > avg_y) {
+        return { x: p.x - 1, y: p.y - 1 };
+      } else if (p.x < avg_x && p.y == avg_y) {
+        return { x: p.x + 1, y: p.y - 1 };
+      } else if (p.x < avg_x && p.y < avg_y) {
+        return { x: p.x + 1, y: p.y };
+      } else if (p.x < avg_x && p.y > avg_y) {
+        return { x: p.x - 1, y: p.y - 1 };
+      } else if (p.x > avg_x && p.y == avg_y) {
+        return { x: p.x - 1, y: p.y + 1 };
+      } else if (p.x > avg_x && p.y < avg_y) {
+        return { x: p.x - 1, y: p.y + 1 };
+      } else if (p.x > avg_x && p.y > avg_y) {
+        return { x: p.x - 1, y: p.y };
+      }
+    });
+
+    setCurrentBlock({ points: new_points });
+  };
+
   const moveDown = () => {
+    if (currentBlock.points.filter((p) => p.y >= FIELD_SIZE_Y - 1).length > 0) {
+      // TODO: тут будет много проверок
+
+      console.log(currentBlock);
+      // блок дошёл до конца
+      return;
+    }
     const new_points: Point[] = currentBlock.points.map((p) => {
       return { x: p.x, y: p.y + 1 };
     });
+
     setCurrentBlock({ points: new_points });
   };
 
@@ -63,22 +103,40 @@ export const App = () => {
 
   useEffect(() => {
     newBlock();
+  }, []);
+
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      console.log(event.key);
+      if (event.key == "ArrowLeft") {
+        moveLeft();
+      } else if (event.key == "ArrowRight") {
+        moveRight();
+      } else if (event.key == " ") {
+        rotate();
+      } else {
+        console.log(event.key);
+      }
+    };
+
+    console.log("set timeout");
+    // const timeoutDown = setTimeout(() => {
+    //   moveDown();
+    // }, 1000);
 
     window.addEventListener("keydown", handleKeyPress);
     return () => {
+      console.log("clear timeout");
       window.removeEventListener("keydown", handleKeyPress);
+      //clearTimeout(timeoutDown);
     };
-  }, []);
-
-  setTimeout(() => {
-    moveDown();
-  }, 1000);
+  }, [currentBlock]);
 
   const blocks = () => {
-    const divs = new Array(20 * 10);
+    const divs = new Array(FIELD_SIZE_X * FIELD_SIZE_Y);
 
-    for (let i = 0; i < 20; i++) {
-      for (let j = 0; j < 10; j++) {
+    for (let i = 0; i < FIELD_SIZE_Y; i++) {
+      for (let j = 0; j < FIELD_SIZE_X; j++) {
         divs.push(
           <div
             className="fieldBlock"
@@ -107,6 +165,10 @@ export const App = () => {
 
       <FixedLayout vertical="bottom" filled style={{ backgroundColor: "blueviolet", height: "100px" }}>
         <Text>Элементы управления</Text>
+        <ButtonGroup>
+          <Button onClick={moveLeft}>Влево</Button>
+          <Button onClick={moveRight}>Вправо</Button>
+        </ButtonGroup>
       </FixedLayout>
     </SplitLayout>
   );
